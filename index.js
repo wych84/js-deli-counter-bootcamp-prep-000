fcf7d593e3995dc96ff2903358af4ad6c1f2d34b
Joyce Chun <wooyangee@gmail.com>
2018-04-25 04:45:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function currentLine (KatzDeliLine) {
   if (KatzDeliLine.length > 0) {
     var line = [];
     for (let i=0; i<KatzDeliLine.length; i++)
-    line.push(" " + (i+1) + ". " + )
+    line.push(" " + (i+1) + ". " + KatzDeliLine[i])
     return 
   }
   

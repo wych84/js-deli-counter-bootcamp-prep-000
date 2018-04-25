ed29be9a966e0d69ccd0afa878cae8d9ce78a153
Joyce Chun <wooyangee@gmail.com>
2018-04-25 04:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber (KatzDeliLine, name) {
-  KatzDeliLine.push(name)
-  return "Welcome, " + name + "." + " You are number " + KatzDeliLine.length + " in line."
+  KatzDeliLine.push(name);
+  return "Welcome, " + name + "." + " You are number " + KatzDeliLine.length + " in line.";
 }

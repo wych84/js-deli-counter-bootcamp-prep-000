eaf03c1dae496ef5bfb3a69e3a125cd81217a1af
Joyce Chun <wooyangee@gmail.com>
2018-04-25 04:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber (KatzDeliLine, name) {
 
 function nowServing (person) {
   if (line.length > 0)
-  return "Currently serving " + person.shift + ".";
+  return "Currently serving " + person.shift() + ".";
   else
   return "There is nobody waiting to be served!";
   }

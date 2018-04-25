af5f5b7ce98435201c7b00aaeb91c3328f1a1644
Joyce Chun <wooyangee@gmail.com>
2018-04-25 04:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function takeANumber (KatzDeliLine, names) {
-  
+  KatzDeliLine = [];
+  for (i=0; i<KatzDeliLine.length; i++)
+  KatzDeliLine.push("Welcome, ${names[i]}. You are number ${i + 1} in line.")
 }
